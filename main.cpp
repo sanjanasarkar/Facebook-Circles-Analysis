bb378c07cbbb125a5ded4e52c4f6481c8acd64a8
shivenk78 <kumar.shiven15@gmail.com>
2020-12-02 15:32:28 -0500

    Updated Main

--- main.cpp
+++ main.cpp
@@ -8,12 +8,15 @@ int main() {
     vector<string> lines = FileReader::fileToVector("data/facebook_combined.txt");
 
     vector<Graph::Edge> edges;
+    std::cout << lines.size() << " " << lines[lines.size() - 1] << std::endl;
 
     Vertex max_node = 0;
     for (size_t i = 0; i < lines.size()/2; ++i) {
         Graph::Edge edge(lines[2*i], lines[2*i + 1], 1);
         edges.push_back(edge);
         max_node = std::max(edge.maxVertex(), max_node);
+        /*if (i % 1000 == 0)
+            std::cout << edge << max_node <<std::endl;*/
     }
 
     Graph g(edges, max_node);
