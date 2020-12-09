45e956b6b5e6cd68d992bffc59e113dd01d621fa
Sanjana Sarkar <ssarkar8@linux-a2.ews.illinois.edu>
2020-12-09 01:30:02 -0600

    Small modification to algorithm

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -3,21 +3,11 @@
 
 #include "catch.hpp"
 #include "../src/FileReader.h"
+#include "../src/Graph.h"
 
-
+/* Dummy test case */
 TEST_CASE("Do you even test?") {
 	std::string res = "Hey";
 	// always check "expected" == "actual" --> be consistent
 	REQUIRE("Hey" == res);
 }
-
-// TEST_CASE("Verify that file_to_vector works on a small example") {
-// 	std::vector<std::string> res = file_to_vector("tests/smallSample.txt");
-// 	std::string expected[] = {"hello", "students", "of", "cs", "225", "!!!"};
-	
-// 	REQUIRE(6 == res.size());
-
-// 	for (int i = 0; i < res.size(); i++) {
-// 		REQUIRE(expected[i] == res[i]);
-// 	}
-// }
